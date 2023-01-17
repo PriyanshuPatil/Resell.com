bd413dbd08940bcb08886d6b1a4438db40eeb3b5
PriyanshuPatil <pantuugurjar@gmail.com>
2023-01-17 18:24:50 +0530

    added cra app

---
+++ quikr-bazaar-clone/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   { "Unit-5 ==> Unit-6"}
+    </div>
+  );
+}
+
+export default App;
