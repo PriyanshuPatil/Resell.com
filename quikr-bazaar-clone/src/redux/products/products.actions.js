73bafe574963218d1427596c9b980e9023f3da09
arnabdasrishi <arnabdasrishi@gmail.com>
2023-01-18 20:19:29 +0530

    redux-work in progress

---
+++ quikr-bazaar-clone/src/redux/products/products.actions.js
@@ -0,0 +1,15 @@
+// action type for products
+
+import { getProductsAPI } from "./products.api";
+import { GET_PRODUCTS_ERROR, GET_PRODUCTS_LOADING, GET_PRODUCTS_SUCCESS } from "./products.types"
+
+// Function for getting the products
+export const getProducts = async(dispatch) => {
+    dispatch ({type: GET_PRODUCTS_LOADING});
+    try {
+        let data = await getProductsAPI();
+        dispatch({ type: GET_PRODUCTS_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch ({type: GET_PRODUCTS_ERROR})
+    }
+}
--- quikr-bazaar-clone/src/redux/store.js
+++ quikr-bazaar-clone/src/redux/store.js
@@ -1,9 +1,19 @@
-import {legacy_createStore,combineReducers,compose, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import { themeReducer } from './theme/theme.reducer';
-const reducer=combineReducers({
-    Theme:themeReducer
-})
-const composer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose ;
-export const store = legacy_createStore(reducer,composer(applyMiddleware(thunk)));
+import {
+  legacy_createStore,
+  combineReducers,
+  compose,
+  applyMiddleware,
+} from "redux";
+import thunk from "redux-thunk";
+import { themeReducer } from "./theme/theme.reducer";
 
+const reducer = combineReducers({
+  Theme: themeReducer,
+});
+
+const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = legacy_createStore(
+  reducer,
+  composer(applyMiddleware(thunk))
+);
