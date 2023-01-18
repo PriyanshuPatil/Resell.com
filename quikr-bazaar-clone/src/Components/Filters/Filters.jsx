016856d5dcd7faa6ca5f3a067f0e40e2bd0b9c4e
harry-io <112643733+harry-io@users.noreply.github.com>
2023-01-18 21:58:02 +0530

    Filters component added

---
+++ quikr-bazaar-clone/src/Components/Filters/Filters.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "./Filters.css";
+
+const Filters = () => {
+  return (
+    <div className="filter_main_container">
+      <div className="filter_head">Filter By</div>
+      <hr style={{ width: "90%" }} />
+      {/*  */}
+      <div className="filter_ctg">
+        <div>Brand Name</div>
+        <div>
+          <input type="checkbox" />
+          <p>Samsung</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>Apple</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>OnePlus</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>Boat</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>Poco</p>
+        </div>
+        {/*  */}
+      </div>
+      {/*  */}
+      <div className="filter_ctg">
+        <div>Price Range</div>
+        <div>
+          <input type="checkbox" />
+          <p>₹500 & Below</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>₹501 - ₹1,000</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>₹1,001 - ₹10,000</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>₹10,000 - ₹50,000</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>₹50,000 & Above</p>
+        </div>
+        {/*  */}
+      </div>
+      {/*  */}
+      <div className="filter_ctg">
+        <div>Operation System</div>
+        <div>
+          <input type="checkbox" />
+          <p>Android</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>IOS</p>
+        </div>
+        {/*  */}
+        <div>
+          <input type="checkbox" />
+          <p>Blackberry</p>
+        </div>
+
+        {/*  */}
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
