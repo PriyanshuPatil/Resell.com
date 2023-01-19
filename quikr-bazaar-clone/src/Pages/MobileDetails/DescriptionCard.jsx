2ebb89db7f0215d8b115876b2f5e673ec628fd1b
harry-io <112643733+harry-io@users.noreply.github.com>
2023-01-19 21:58:01 +0530

    Mobile-Details children component added

---
+++ quikr-bazaar-clone/src/Pages/MobileDetails/DescriptionCard.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Description.css";
+import { BsDot } from "react-icons/bs";
+import { RxDividerVertical } from "react-icons/rx";
+
+const DescriptionCard = ({ desc }) => {
+  return (
+    <div>
+      <div className="desc_heading_a">
+        <h4>USED</h4>
+        <BsDot style={{ fontSize: "2em", color: "#5b7194" }} />
+        <h4>{desc.parameters[0].value_name}</h4>
+      </div>
+      {/*  */}
+      <div className="desc_heading_b">
+        <h1>{desc.title}</h1>
+      </div>
+      {/*  */}
+      <div className="desc_location">
+        <p>{desc.locations_resolved.ADMIN_LEVEL_1_name}</p>
+        <RxDividerVertical style={{ fontSize: "1rem" }} />
+        <p>{`Posted on ${desc.created_at_first.slice(0, 10)}`}</p>
+      </div>
+      {/*  */}
+      <div className="desc_price">
+        <h1>{desc.price.value.display}</h1>
+      </div>
+      {/*  */}
+      <div className="desc_buttons">
+        <button>ADD TO CART</button>
+        <button>CHAT</button>
+      </div>
+      {/*  */}
+      <div className="desc_desc">
+        <div>
+          <div>
+            <img
+              src="https://teja9.kuikr.com/core/clsfd/assets/ic-get-all-details-2x.png"
+              alt="img"
+            />
+          </div>
+          <p>Get all details</p>
+        </div>
+        <div>
+          <div>
+            <img
+              src="https://teja9.kuikr.com/core/clsfd/assets/ic-schedule-visit-bazaar-2x.png"
+              alt="img"
+            />
+          </div>
+          <p>Shedule a visit</p>
+        </div>
+        <div>
+          <div>
+            <img
+              src="https://teja9.kuikr.com/core/clsfd/assets/ic-negotiate-2x.png"
+              alt="img"
+            />
+          </div>
+          <p>Negotiate price</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionCard;
