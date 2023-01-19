ed1f9c24f946b726be3ee0fdb0c050b79c8404f8
harry-io <112643733+harry-io@users.noreply.github.com>
2023-01-19 22:15:17 +0530

    Specifications component added

---
+++ quikr-bazaar-clone/src/Pages/MobileDetails/Components/Specifications.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Specifications.css";
+const Specifications = ({ details }) => {
+  return (
+    <div>
+      <div>
+        <h1 className="specifications_heading">Specifications</h1>
+      </div>
+      {/*  */}
+
+      <div className="specification_main_inner">
+        <h3>No Specifications available</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Specifications;
