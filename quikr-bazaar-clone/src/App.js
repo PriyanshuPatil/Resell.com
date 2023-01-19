373837462ba1ea37f72bda41ae8d1809615980b4
PriyanshuPatil <pantuugurjar@gmail.com>
2023-01-19 13:14:08 +0530

    edeted app file

--- quikr-bazaar-clone/src/App.js
+++ quikr-bazaar-clone/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { applyDarkTheme, applyLightTheme } from './redux/theme/theme.actions';
 import { useDispatch, useSelector } from "react-redux";
-import PostAd from './Components/PostAd/PostAd';
 function App() {
   const dispatch=useDispatch();
   const theme=useSelector((store)=>store.Theme.theme)
@@ -11,7 +10,6 @@ function App() {
     <div >
       {/* navbar  */}
       <Navbar />
-      <PostAd/>
   {/* {"Unit-5  =>  UNIT-6"} */}
     </div>
   );
